bb26de909a6cb8822e604fa54714cd471e0c3c04
Bruna Nicolau <brunanicolau456@gmail.com>
2023-12-26 10:43:05 -0300

    tela de att status 100%

--- src/app/pages/receipt/update-receipt/update-receipt.component.ts
+++ src/app/pages/receipt/update-receipt/update-receipt.component.ts
@@ -29,16 +29,21 @@ export class UpdateReceiptComponent {
     });
   }
 
-  searchReceipt(id: any) {
+  searchReceipt(event: any) {
     // TODO: passar os dados do escritorio via header
-    this.receiptService.receiptById(id.receiptID).subscribe({
-      next: (res) => {
-        this.fillInput(res);
-      },
-      error: (error) => {
-        this.snackBar.open(error.message, '', { duration: 5000 });
-      },
-    });
+    if (event.target.value) {
+      this.receiptService.receiptById(event.target.value).subscribe({
+        next: (res) => {
+          this.fillInput(res);
+        },
+        error: (error) => {
+          this.snackBar.open(error.error.message, '', { duration: 5000 });
+          this.cleanFields();
+        },
+      });
+    } else {
+      this.cleanFields();
+    }
   }
 
   fillInput(receiptData: any) {
@@ -46,35 +51,65 @@ export class UpdateReceiptComponent {
     this.updateReceiptForm.get('status')?.setValue(receipt.status);
     this.updateReceiptForm.get('receiptDate')?.setValue(receipt.fim);
 
-    if (receipt.status != 'E') {
+    if (receipt.status == 'E') {
+      this.updateReceiptForm.get('status')?.enable();
+      this.updateReceiptForm.get('receiptDate')?.disable();
+    } else if (receipt.status == 'C') {
       this.updateReceiptForm.get('status')?.disable();
       this.updateReceiptForm.get('receiptDate')?.disable();
-    } else {
-      this.updateReceiptForm.get('status')?.enable();
-      this.updateReceiptForm.get('receiptDate')?.enable();
+    } else if (receipt.status == 'R') {
+      this.updateReceiptForm.get('status')?.disable();
+      this.updateReceiptForm.get('receiptDate')?.setValue(receipt.fim);
+      this.updateReceiptForm.get('receiptDate')?.disable();
     }
+
+    this.verifyInputValue();
   }
 
-  controllInput(option: any){
-    //TODO: nao esta funcionando 
-    console.log('passou aq:', option);
-    if(option === 'E') this.updateReceiptForm.get('receiptDate')?.setValidators([Validators.required])
-    else if(option === 'C') this.updateReceiptForm.get('receiptDate')?.disable();
+  verifyInputValue() {
+    const receiptDateControl = this.updateReceiptForm.get('receiptDate');
 
+    this.updateReceiptForm.get('status')?.valueChanges.subscribe((newValue) => {
+      if (newValue === 'E') {
+        receiptDateControl?.setValue('');
+        receiptDateControl?.disable();
+        receiptDateControl?.setValidators(null);
+      } else if (newValue === 'C') {
+        receiptDateControl?.enable();
+        receiptDateControl?.setValidators([Validators.required]);
+      } else if (newValue === 'R') {
+        receiptDateControl?.enable();
+        receiptDateControl?.setValidators([Validators.required]);
+      }
+    });
+  }
+
+  isSubmitButtonDisabled() {
+    const status = this.updateReceiptForm.get('status')?.value;
+    const receiptDate = this.updateReceiptForm.get('receiptDate')?.value;
+
+    if ((status === 'R' && receiptDate) || (status === 'C' && receiptDate))
+      return false;
+
+    return true;
   }
 
   confirm(req: any) {
     this.receiptService.updateReceipt(req).subscribe({
       next: (res) => {
         this.snackBar.open(res.txt + res.id, '', { duration: 5000 });
-        this.updateReceiptForm.get('receiptID')?.setValue('');
-        this.updateReceiptForm.get('status')?.setValue('');
-        this.updateReceiptForm.get('receiptDate')?.setValue('');
+        this.cleanFields();
       },
       error: (error) => {
-        this.snackBar.open(error.message, '', { duration: 5000 });
+        this.snackBar.open(error.error.message, '', { duration: 5000 });
       },
     });
+  }
+
+  cleanFields() {
+    this.updateReceiptForm.get('receiptID')?.setValue('');
+    this.updateReceiptForm.get('status')?.setValue('');
+    this.updateReceiptForm.get('receiptDate')?.setValue('');
   }
 
   backToLastPage() {
